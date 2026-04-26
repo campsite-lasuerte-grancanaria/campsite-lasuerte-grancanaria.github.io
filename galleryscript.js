6e309ea99f1338f8239f6ef862166b0a34ba9fa0
Anna <anna-sp@mailfence.com>
2026-04-26 20:36:15 +0100

    Add files via upload

--- galleryscript.js
+++ galleryscript.js
@@ -1,92 +1,74 @@
 let photocounter;
+let folder;
+let numberOfPhotos;
 
-const photofolder = (numberOfPhotos) => {
-    return {
-        numberOfPhotos
-    }
-};
-
-const gallery = photofolder(20);
-const defender = photofolder(12);
-const bus = photofolder(6);
-const studio = photofolder(8);
-const tent = photofolder(5);
-const space = photofolder(4);
-const kitchen = photofolder(7);
-const toilet = photofolder(3);
-const shower = photofolder(3);
-
-function startphotoshow(foldername, number) {
+function startphotoshow(number) {
     photocounter = number;
-    currentFolder = foldername;
     document.getElementById("photoshow").style.display = "grid";
-    document.getElementById("shownphoto").src = `./images/${foldername}/${photocounter}.jpg`;
+    document.getElementById("shownphoto").src = `./images/${folder}/${photocounter}.jpg`;
 }
 
-function changephoto(currentFolder, photonumber, x) {
+function changephoto(x) {
     photocounter += x;
     
     if (photocounter < 1) {
-        photocounter = photonumber;
-    } else if (photocounter > photonumber) {
+        photocounter = numberOfPhotos;
+    } else if (photocounter > numberOfPhotos) {
         photocounter = 1;
     }
-    document.getElementById("shownphoto").src = `./images/${currentFolder}/${photocounter}.jpg`;
+    document.getElementById("shownphoto").src = `./images/${folder}/${photocounter}.jpg`;
 }
+
 
 document.getElementById("closebutton").onclick = function() {
     document.getElementById("photoshow").style.display = "none";
 }
 
+document.getElementById("previousphoto").onclick = function() {
+    changephoto(-1);
+}
+document.getElementById("nextphoto").onclick = function() {
+    changephoto(1);
+}
+
 
 // gallery
 
-if (document.getElementById("previousphotogallery")) {
+if (document.getElementById("gallery1")) {
 
-document.getElementById("previousphotogallery").onclick = function() {
-    let photonumber = gallery.numberOfPhotos;
-    changephoto('gallery', photonumber, -1);
-}
-document.getElementById("nextphotogallery").onclick = function() {
-    let photonumber = gallery.numberOfPhotos;
-    changephoto('gallery', photonumber, 1);
-}
+    folder = 'gallery';
+    numberOfPhotos = 20;
 
-for (i=1; i<= 8; i++) {
-    let number = i;
-    document.getElementById(`gallery${i}`).onclick = function() {
-        startphotoshow('gallery', number);
-    }
-};
-
-if (document.getElementById("gallery9")) {
-    for (i=9; i<= gallery.numberOfPhotos; i++) {
+    for (i=1; i<= 8; i++) {
         let number = i;
-        document.getElementById(`gallery${i}`).onclick = function() {
-            startphotoshow('gallery', number);
+        document.getElementById(`gallery${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
-}
-}
+
+    if (document.getElementById("gallery9")) {
+        for (i=9; i<= numberOfPhotos; i++) {
+            let number = i;
+            document.getElementById(`gallery${number}`).onclick = function() {
+                startphotoshow(number);
+            }
+        };
+    } 
+    
+};
 
 
 // defender
 
-if (document.getElementById("previousphotodefender")) {
+if (document.getElementById("defender1")) {
 
-    document.getElementById("previousphotodefender").onclick = function() {
-        let photonumber = defender.numberOfPhotos;
-        changephoto('defender', photonumber, -1);
-    }
-    document.getElementById("nextphotodefender").onclick = function() {
-        let photonumber = defender.numberOfPhotos;
-        changephoto('defender', photonumber, 1);
-    }
-    
-    for (i=1; i<= defender.numberOfPhotos; i++) {
+    folder = 'defender';
+    numberOfPhotos = 12;
+
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`defender${i}`).onclick = function() {
-            startphotoshow('defender', number);
+        document.getElementById(`defender${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
@@ -94,21 +76,15 @@ if (document.getElementById("previousphotodefender")) {
 
 // bus
 
-if (document.getElementById("previousphotobus")) {
+if (document.getElementById("bus1")) {
 
-    document.getElementById("previousphotobus").onclick = function() {
-        let photonumber = bus.numberOfPhotos;
-        changephoto('bus', photonumber, -1);
-    }
-    document.getElementById("nextphotobus").onclick = function() {
-        let photonumber = bus.numberOfPhotos;
-        changephoto('bus', photonumber, 1);
-    }
+    folder = 'bus';
+    numberOfPhotos = 6;
     
-    for (i=1; i<= bus.numberOfPhotos; i++) {
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`bus${i}`).onclick = function() {
-            startphotoshow('bus', number);
+        document.getElementById(`bus${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
@@ -116,21 +92,15 @@ if (document.getElementById("previousphotobus")) {
 
 // studio
 
-if (document.getElementById("previousphotostudio")) {
+if (document.getElementById("studio1")) {
 
-    document.getElementById("previousphotostudio").onclick = function() {
-        let photonumber = studio.numberOfPhotos;
-        changephoto('studio', photonumber, -1);
-    }
-    document.getElementById("nextphotostudio").onclick = function() {
-        let photonumber = studio.numberOfPhotos;
-        changephoto('studio', photonumber, 1);
-    }
+    folder = 'studio';
+    numberOfPhotos = 8;
     
-    for (i=1; i<= studio.numberOfPhotos; i++) {
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`studio${i}`).onclick = function() {
-            startphotoshow('studio', number);
+        document.getElementById(`studio${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
@@ -138,21 +108,15 @@ if (document.getElementById("previousphotostudio")) {
 
 // tent
 
-if (document.getElementById("previousphototent")) {
+if (document.getElementById("tent1")) {
 
-    document.getElementById("previousphototent").onclick = function() {
-        let photonumber = tent.numberOfPhotos;
-        changephoto('tent', photonumber, -1);
-    }
-    document.getElementById("nextphototent").onclick = function() {
-        let photonumber = tent.numberOfPhotos;
-        changephoto('tent', photonumber, 1);
-    }
-    
-    for (i=1; i<= tent.numberOfPhotos; i++) {
+    folder = 'tent';
+    numberOfPhotos = 5;
+
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`tent${i}`).onclick = function() {
-            startphotoshow('tent', number);
+        document.getElementById(`tent${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
@@ -160,21 +124,15 @@ if (document.getElementById("previousphototent")) {
 
 // tentspace
 
-if (document.getElementById("previousphotospace")) {
+if (document.getElementById("space1")) {
 
-    document.getElementById("previousphotospace").onclick = function() {
-        let photonumber = space.numberOfPhotos;
-        changephoto('space', photonumber, -1);
-    }
-    document.getElementById("nextphotospace").onclick = function() {
-        let photonumber = space.numberOfPhotos;
-        changephoto('space', photonumber, 1);
-    }
-    
-    for (i=1; i<= space.numberOfPhotos; i++) {
+   folder = 'space';
+   numberOfPhotos = 4;
+
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`space${i}`).onclick = function() {
-            startphotoshow('space', number);
+        document.getElementById(`space${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
@@ -182,23 +140,15 @@ if (document.getElementById("previousphotospace")) {
 
 // kitchen
 
-if (document.getElementById("previousphotokitchen")) {
+if (document.getElementById("kitchen1")) {
 
-    document.getElementById("previousphotokitchen").onclick = function() {
-        let photonumber = kitchen.numberOfPhotos;
-        changephoto('kitchen', photonumber, -1);
-    }
-    document.getElementById("nextphotokitchen").onclick = function() {
-        let photonumber = kitchen.numberOfPhotos;
-        changephoto('kitchen', photonumber, 1);
-    }
-    
-    for (i=1; i<= kitchen.numberOfPhotos; i++) {
+    folder = 'kitchen';
+    numberOfPhotos = 7;
+
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`kitchen${i}`).onclick = function() {
-            photocounter = number;
-            startphotoshow('kitchen', number);
-            document.getElementById("photoshow").style.display = "grid";
+        document.getElementById(`kitchen${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
@@ -206,23 +156,15 @@ if (document.getElementById("previousphotokitchen")) {
 
 // toilet
 
-if (document.getElementById("previousphototoilet")) {
+if (document.getElementById("toilet1")) {
 
-    document.getElementById("previousphototoilet").onclick = function() {
-        let photonumber = toilet.numberOfPhotos;
-        changephoto('toilet', photonumber, -1);
-    }
-    document.getElementById("nextphototoilet").onclick = function() {
-        let photonumber = toilet.numberOfPhotos;
-        changephoto('toilet', photonumber, 1);
-    }
-    
-    for (i=1; i<= toilet.numberOfPhotos; i++) {
+    folder = 'toilet';
+    numberOfPhotos = 3;
+
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`toilet${i}`).onclick = function() {
-            photocounter = number;
-            startphotoshow('toilet', number);
-            document.getElementById("photoshow").style.display = "grid";
+        document.getElementById(`toilet${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }   
@@ -230,23 +172,15 @@ if (document.getElementById("previousphototoilet")) {
 
 // shower
 
-if (document.getElementById("previousphotoshower")) {
+if (document.getElementById("shower1")) {
 
-    document.getElementById("previousphotoshower").onclick = function() {
-        let photonumber = shower.numberOfPhotos;
-        changephoto('shower', photonumber, -1);
-    }
-    document.getElementById("nextphotoshower").onclick = function() {
-        let photonumber = shower.numberOfPhotos;
-        changephoto('shower', photonumber, 1);
-    }
+    folder = 'shower';
+    numberOfPhotos = 3;
     
-    for (i=1; i<= shower.numberOfPhotos; i++) {
+    for (i=1; i<= numberOfPhotos; i++) {
         let number = i;
-        document.getElementById(`shower${i}`).onclick = function() {
-            photocounter = number;
-            startphotoshow('shower', number);
-            document.getElementById("photoshow").style.display = "grid";
+        document.getElementById(`shower${number}`).onclick = function() {
+            startphotoshow(number);
         }
     };
 }
